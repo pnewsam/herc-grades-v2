a8ae82e505d9900b40d028af4280a3f9f498150e
monkypuzzle <paul.newsam@gmail.com>
2017-09-13 11:36:37 -0700

    Adds Login components, minor changes to Signup

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -3,15 +3,14 @@ import axios from "axios";
 import FormField from "../bulma/FormField";
 import CenteredCard from "../bulma/CenteredCard";
 
-class SignUpForm extends Component {
+class LoginForm extends Component {
   constructor(props){
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       email: "",
-      password: "",
-      password_confirmation: ""
+      password: ""
     }
   }
 
@@ -23,13 +22,10 @@ class SignUpForm extends Component {
 
   handleSubmit(e){
     e.preventDefault();
-    let d = {
+    axios.post('/auth/sign_in', {
       email: this.state.email,
-      password: this.state.password,
-      password_confirmation: this.state.password_confirmation,
-      confirm_success_url: "/"
-    }
-    axios.post('/auth', d)
+      password: this.state.password
+    })
     .then(response => console.log(response))
     .catch(error => console.log(error));
   }
@@ -37,14 +33,13 @@ class SignUpForm extends Component {
   render(){
     return(
       <CenteredCard>
-        <h1 className="title is-1 has-text-centered">Sign up for an account</h1>
+        <h1 className="title is-1 has-text-centered">Log in to your account</h1>
         <hr/>
         <form onSubmit={this.handleSubmit}>
           <FormField label="Email">
             <input
               className="input"
               id="email"
-              // name="email"
               type="text"
               placeholder="Enter email here..."
               value={this.state.email}
@@ -55,21 +50,9 @@ class SignUpForm extends Component {
             <input
               className="input"
               id="password"
-              // name="password"
               type="password"
               placeholder="Enter password here..."
               value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </FormField>
-          <FormField label="Confirm Password">
-            <input
-              className="input"
-              id="password_confirmation"
-              // name="password_confirmation"
-              type="password"
-              placeholder="Retype password here..."
-              value={this.state.password_confirmation}
               onChange={this.handleChange}
             />
           </FormField>
@@ -80,4 +63,4 @@ class SignUpForm extends Component {
   }
 }
 
-export default SignUpForm;
+export default LoginForm;

b33a2ed71af38a4f2fcd7a60b1f21f78696b91b3
monkypuzzle <paul.newsam@gmail.com>
2017-09-13 21:24:07 -0700

    Adds redux-logger to dev env

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import reducers from "./reducers";
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import "bulma/css/bulma.css";
 
-export let store = createStore(reducers);
+export let store = createStore(reducers, applyMiddleware(logger));
 
 render(
   <Provider store={store}>
@@ -18,8 +19,3 @@ render(
 );
 
 registerServiceWorker();
-
-store.subscribe(() => {
-  console.log("Store changed!", store.getState())
-})
-

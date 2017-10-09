3fc7c30252948891e277d29df149b3fff7ddc0c9
monkypuzzle <paul.newsam@gmail.com>
2017-10-09 15:25:28 -0700

    Implements course creation via form

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,7 +1,19 @@
 class Api::CoursesController < ApplicationController
   before_action :authenticate_teacher!
   def create
-    @courses = Course.all
-    render json: @courses.to_json
+    c = Course.new(name: params[:name])
+    if c.save
+      render json: {
+        data: {
+          message: "Course successfully created!",
+        }
+      }, status: 200
+    else
+      render json: {
+        data: {
+          message: 'Something went wrong.'
+        }, status: 500
+      }
+    end
   end
 end

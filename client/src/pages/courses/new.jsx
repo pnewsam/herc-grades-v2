90a99e2a5b86702834d47dd668c0572d230bdfc2
monkypuzzle <paul.newsam@gmail.com>
2017-10-16 18:24:21 -0400

    Moves NewCourseForm into own component

--- client/src/pages/courses/new.jsx
+++ client/src/pages/courses/new.jsx
@@ -1,66 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
-import { headers } from '../../utils/apiUtil';
-import { setBrowserSession } from '../../utils/authUtil';
-
-import FormField from '../../components/bulma/FormField';
+import NewCourseForm from '../../components/NewCourseForm/NewCourseForm';
 
 const New = () => {
   return (
     <div>
       <h2 className="title is-2">Create a New Course</h2>
-      <CourseForm />
+      <NewCourseForm />
     </div>
   );
 };
-
-class CourseForm extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      name: ''
-    };
-  }
-
-  handleChange(e) {
-    this.setState({
-      [e.target.getAttribute('name')]: e.target.value
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    axios({
-      method: 'POST',
-      url: '/api/courses',
-      data: new FormData(e.target),
-      headers: headers()
-    })
-      .then(response => {
-        console.log(response);
-        setBrowserSession(response);
-      })
-      .catch(error => console.log(error));
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <FormField label="name">
-          <input
-            type="text"
-            name="name"
-            className="input"
-            onChange={this.handleChange}
-            value={this.state.name}
-          />
-        </FormField>
-        <button className="button is-primary">Submit</button>
-      </form>
-    );
-  }
-}
 
 export default New;

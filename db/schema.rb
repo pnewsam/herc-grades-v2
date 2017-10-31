1776e440cdffb17e47214591435bfa79da6f00ca
monkypuzzle <paul.newsam@gmail.com>
2017-10-31 14:22:18 -0700

    Adds updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030212810) do
+ActiveRecord::Schema.define(version: 20171031205921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20171030212810) do
   create_table "sections", force: :cascade do |t|
     t.string "name", null: false
     t.integer "teacher_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "section_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
